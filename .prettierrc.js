df3950113cb82dbd94d4e73506a856fe6321c330
codeofdolphins-dev <pdas2142000@gmail.com>
2026-01-17 11:08:38 +0530

    feat: 🎸 new version added

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  singleQuote: true,
-  trailingComma: 'all',
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: true,
+    semi: true,
+    trailingComma: 'all',
+    printWidth: 80,
 };
